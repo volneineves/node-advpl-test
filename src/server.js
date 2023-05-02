47159a5628a6fc81c6570647fce96546cf35784d
Volnei Neves <volneinevesfilho@hotmail.com>
2023-05-02 17:42:30 -0300

    First commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.text({type: 'application/xml'}));
+
+
+
+app.post("/test", express.text(), (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.listen(3000, () => {
+  console.log("Servidor iniciado na porta 3000.");
+});
